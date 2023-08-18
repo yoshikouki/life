df0bcdb55daf24a266e4fa82416b9ebc6e60877c
yoshikouki <yoshikouki@gmail.com>
2023-08-18 22:30:11 +0900

    Enable MDX plugins for RSC

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -1,5 +1,7 @@
 import { fetchMarkdown } from "@/lib/fetch-markdown";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
 import { components } from "./MarkdownComponents";
 
 interface Props {
@@ -8,7 +10,18 @@ interface Props {
 
 const MarkdownContent = async ({ path }: Props) => {
   const markdown = await fetchMarkdown(path);
-  return <MDXRemote source={markdown} components={components} />;
+  return (
+    <MDXRemote
+      source={markdown}
+      components={components}
+      options={{
+        mdxOptions: {
+          remarkPlugins: [remarkGfm, remarkBreaks],
+          rehypePlugins: [],
+        },
+      }}
+    />
+  );
 };
 
 export default MarkdownContent;
