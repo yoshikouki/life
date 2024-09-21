e58fe5c2992ffa436ef445946680d81c2cfe3638
yoshikouki <yoshikouki@pepabo.com>
2024-09-21 16:08:42 +0900

    Remove all

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MarkdownContent from "@/components/MarkdownContent";
-
-export default async function Page({
-  params: { slug },
-}: {
-  params: { slug: string[] };
-}) {
-  return <MarkdownContent path={"/"} />;
-}
