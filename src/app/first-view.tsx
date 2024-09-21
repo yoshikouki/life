132e6cf83618e2f403fc23b123047a58f7f61a2c
yoshikouki <yoshikouki@pepabo.com>
2024-09-22 00:45:58 +0900

    Create first view animation

---
+++ src/app/first-view.tsx
@@ -0,0 +1,59 @@
+import * as motion from "framer-motion/client";
+
+export const FirstView = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 1 }}
+      animate={{
+        opacity: 0,
+      }}
+      transition={{
+        delay: 2,
+        duration: 0.5,
+      }}
+      style={{
+        position: "fixed",
+        inset: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "hsl(var(--background))",
+      }}
+    >
+      <div className="flex items-center justify-center">
+        {"yoshikouki".split("").map((char, index) => (
+          <motion.span
+            initial={{ visibility: "hidden", width: 0 }}
+            animate={{ visibility: "visible", width: "auto" }}
+            transition={{
+              delay: index * 0.1,
+              duration: 0,
+            }}
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={`${char}-${index}`}
+          >
+            {char}
+          </motion.span>
+        ))}
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+          }}
+          transition={{
+            duration: 0.5,
+            ease: "anticipate",
+            repeat: Number.POSITIVE_INFINITY,
+            repeatType: "mirror",
+          }}
+          style={{
+            marginLeft: "0.1rem",
+            width: 1,
+            height: "1rem",
+            backgroundColor: "hsl(12 6.5% 55.1%)",
+          }}
+        />
+      </div>
+    </motion.div>
+  );
+};
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,12 @@
+import { FirstView } from "./first-view";
+
 export const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-50">
       <div className="flex min-h-20 items-center justify-between px-4 py-2">
         <div>yoshikouki</div>
       </div>
+      <FirstView />
     </header>
   );
 };
