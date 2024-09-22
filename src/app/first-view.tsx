9f8a7665cb0a9c64bf5807d7036dfed5f63af4c8
yoshikouki <yoshikouki@pepabo.com>
2024-09-22 11:15:54 +0900

    Moving logo

--- src/app/first-view.tsx
+++ src/app/first-view.tsx
@@ -1,58 +1,73 @@
 import * as motion from "framer-motion/client";
-import { FirstViewOverlay } from "./first-view-overlay";
 
 export const FirstView = () => {
   return (
-    <FirstViewOverlay
-      initial={{ opacity: 1 }}
-      animate={{ opacity: 0 }}
+    <motion.div
+      initial={{
+        height: "100svh",
+        backgroundColor: "hsla(20, 14.3%, 4.1%, 1)",
+      }}
+      animate={{ height: "100%", backgroundColor: "hsla(20, 14.3%, 4.1%, 0)" }}
       transition={{
         delay: 2,
-        duration: 0.5,
+        duration: 0.3,
+        ease: "easeInOut",
       }}
       style={{
-        position: "fixed",
-        inset: 0,
         display: "flex",
         alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "hsl(var(--background))",
+        justifyContent: "start",
+        width: "100vw",
       }}
     >
-      <div className="flex items-center justify-center">
-        {"yoshikouki".split("").map((char, index) => (
-          <motion.span
-            initial={{ visibility: "hidden", width: 0 }}
-            animate={{ visibility: "visible", width: "auto" }}
-            transition={{
-              delay: index * 0.1,
-              duration: 0,
+      <motion.div
+        initial={{ width: "100%" }}
+        animate={{ width: "auto" }}
+        transition={{
+          delay: 2,
+          duration: 0.3,
+          ease: "easeInOut",
+        }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <motion.div layout className="flex items-center">
+          {"yoshikouki".split("").map((char, index) => (
+            <motion.div
+              initial={{ visibility: "hidden", width: 0 }}
+              animate={{ visibility: "visible", width: "auto" }}
+              transition={{
+                delay: 1 + index * 0.1,
+                duration: 0,
+              }}
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              key={`${char}-${index}`}
+            >
+              {char}
+            </motion.div>
+          ))}
+          <motion.div
+            animate={{
+              opacity: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
             }}
-            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-            key={`${char}-${index}`}
-          >
-            {char}
-          </motion.span>
-        ))}
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{
-            opacity: 0,
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "anticipate",
-            repeat: 5,
-            repeatType: "mirror",
-          }}
-          style={{
-            marginLeft: "0.1rem",
-            width: 1,
-            height: "1rem",
-            backgroundColor: "hsl(12 6.5% 55.1%)",
-          }}
-        />
-      </div>
-    </FirstViewOverlay>
+            transition={{
+              duration: 4,
+              ease: "anticipate",
+              repeatType: "mirror",
+            }}
+            style={{
+              marginLeft: "0.1rem",
+              opacity: 0,
+              width: 1,
+              height: "1rem",
+              backgroundColor: "hsl(12 6.5% 55.1%)",
+            }}
+          />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,9 +4,8 @@ export const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-50">
       <div className="flex min-h-20 items-center justify-between px-4 py-2">
-        <div>yoshikouki</div>
+        <FirstView />
       </div>
-      <FirstView />
     </header>
   );
 };
