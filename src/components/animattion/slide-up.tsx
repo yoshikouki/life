3f43328a131350677e39d73d0807e2a281eb84f9
yoshikouki <yoshikouki@pepabo.com>
2024-09-26 19:54:49 +0900

    Refine slide-up animation

--- src/components/animattion/slide-up.tsx
+++ src/components/animattion/slide-up.tsx
@@ -3,9 +3,9 @@ import * as motion from "framer-motion/client";
 export const SlideUp = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 10 }}
+      initial={{ opacity: 0, y: "50%" }}
       whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.1 }}
+      transition={{ duration: 0.2 }}
     >
       {children}
     </motion.div>
