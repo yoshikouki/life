c9ca767bd3837f1752d51290683a76d13237f1f0
yoshikouki <yoshikouki@pepabo.com>
2024-09-26 22:23:23 +0900

    Clear AnimatedLink

--- src/components/animated-link.tsx
+++ src/components/animated-link.tsx
@@ -1,19 +1,3 @@
-"use client";
+import { Link } from "next-view-transitions";
 
-import { useTransitionRouter } from "next-view-transitions";
-
-export const AnimatedLink = ({
-  children,
-  href,
-  ...props,
-}: {
-  children: React.ReactNode;
-  href: string;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  const router = useTransitionRouter();
-  return (
-    <button type="button" onClick={() => router.push(href)} {...props}>
-      {children}
-    </button>
-  );
-};
+export const AnimatedLink = Link;
